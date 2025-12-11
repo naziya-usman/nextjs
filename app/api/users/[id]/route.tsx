1ed4958a7ad11f5c67ef3c40f8a2e3a56a86e2a1
Naziya Usman <naziyausmantwaiba@gmail.com>
2025-12-11 14:40:17 +0530

    create get post put delete endpoints

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,43 +1,72 @@
 import { NextRequest, NextResponse } from "next/server";
 import UserSchema from "../schema";
-import { prisma } from '../../../lib/prisma';
+import { prisma } from "../../../lib/prisma";
 interface RouteParams {
     id: string;
 }
-export async function GET(request: NextRequest, { params }: { params: Promise<RouteParams> }) {
-const resolvedParams = await params;
-const users= await prisma.user.findUnique({
-    where: {
-        id: parseInt(resolvedParams.id),
-    },
-});
+export async function GET(
+    request: NextRequest,
+    { params }: { params: Promise<RouteParams> }
+) {
+    const resolvedParams = await params;
+    const users = await prisma.user.findUnique({
+        where: {
+            id: parseInt(resolvedParams.id),
+        },
+    });
 
     if (!users)
-        return NextResponse.json({ message: 'User not found' }, { status: 404 });
+        return NextResponse.json({ message: "User not found" }, { status: 404 });
 
     return NextResponse.json(users);
 }
 
-export async function PUT(request: NextRequest, { params }: { params: Promise<RouteParams> }) {
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: Promise<RouteParams> }
+) {
     const resolvedParams = await params;
     const id = parseInt(resolvedParams.id);
     const body = await request.json();
-    try {
-        UserSchema.parse(body);
-    } catch (error) {
-        return NextResponse.json({ message: 'Name is required' }, { status: 400 });
-    }
-    if (id > 10)
-        return NextResponse.json({ message: 'User not found' }, { status: 404 });
-    return NextResponse.json({ id: id, name: body.name }, { status: 200 });
+    const validation = UserSchema.safeParse(body);
+
+    if (!validation.success)
+        return NextResponse.json({ message: "Name is required" }, { status: 400 });
+
+    const user = await prisma.user.findUnique({
+        where: { id: id },
+    });
+    if (!user)
+        return NextResponse.json({ message: "User not found" }, { status: 404 });
+
+    const updatedUser = await prisma.user.update({
+        where: { id: id },
+        data: {
+            name: body.name,
+            email: body.email,
+        },
+    });
+    console.log(updatedUser);
+
+    return NextResponse.json(updatedUser, { status: 200 });
 }
 
-export async function DELETE(request: NextRequest, { params }: { params: Promise<RouteParams> }) {
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: Promise<RouteParams> }
+) {
     const resolvedParams = await params;
     const id = parseInt(resolvedParams.id);
 
-    if (id > 10)
-        return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    const user = await prisma.user.findUnique({ where: { id: id } });
 
-    return NextResponse.json({ message: `User with id ${id} deleted` }, { status: 200 });
+    if (!user)
+        return NextResponse.json({ message: "User not found" }, { status: 404 });
+
+    await prisma.user.delete({ where: { id: id } });
+
+    return NextResponse.json(
+        { message: `the user ${user.name} is deleted` },
+        { status: 200 }
+    );
 }
--- app/api/users/schema.ts
+++ app/api/users/schema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 const UserSchema = z.object({
   name: z.string().min(1, "Name is required"),
+  email: z.string().email()
 });
 
 export default UserSchema;

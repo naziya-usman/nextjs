637d5c9a06447075e35b44d68961efda96dc8616
Naziya Usman <naziyausmantwaiba@gmail.com>
2025-12-19 11:03:16 +0530

    create CredentialsProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,40 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../lib/prisma";
+import bcrypt from "bcrypt";
 
-
-export const authOptions :NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+
+      async authorize(credentials, req) {
+        if (credentials?.email! || credentials?.password!) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email! },
+        });
+        if (!user) return null;
+
+        const isPasswordValid = await bcrypt.compare(
+          credentials?.password!,
+          user.hashedPassword!
+        );
+        return isPasswordValid ? user : null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,

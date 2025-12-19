6a4533f4f7a216af0da3f54d56c2862e3290ba3f
Naziya Usman <naziyausmantwaiba@gmail.com>
2025-12-19 16:08:31 +0530

    add styles in email

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Html, Body, Text, Link, Preview, Container } from '@react-email/components'
+import { Html, Body, Text, Link, Preview, Container, Tailwind } from '@react-email/components'
 
 
 
@@ -7,12 +7,14 @@ const WelcomeTemplate = ({ name }: { name: string }) => {
     return (
         <Html>
             <Preview>Welcome to Our Service!</Preview>
-            <Body>
-                <Container>
-                    <Text> hello {name}</Text>
-                    <Link href="https://naziya.com">www.naziya.com</Link>
-                </Container>
-            </Body>
+            <Tailwind>
+                <Body className="bg-yellow-100 my-auto mx-auto font-sans text-black ">
+                    <Container>
+                        <Text className='text-3xl font-bold'> hello {name}</Text>
+                        <Link href="https://naziya.com">www.naziya.com</Link>
+                    </Container>
+                </Body>
+            </Tailwind>
         </Html>
     )
 }

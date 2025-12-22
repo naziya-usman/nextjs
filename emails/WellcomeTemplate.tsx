aa23e6f066a27321e8b902fad14949130d2df03b
Naziya Usman <naziyausmantwaiba@gmail.com>
2025-12-22 21:42:11 +0530

    create resend and send email

--- emails/WellcomeTemplate.tsx
+++ emails/WellcomeTemplate.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Html, Head, Preview, Body, Container, Heading, Hr, Text, } from '@react-email/components'
-const WellcomeTemplate = () => {
+const WellcomeTemplate = ({ name }: { name: string }) => {
     return (
         <Html>
             <Head />
@@ -9,6 +9,7 @@ const WellcomeTemplate = () => {
                 <Container>
                     <Heading>Welcome to our platform!</Heading>
                     <Hr />
+                    <Text>hello {name}</Text>
                     <Text>Thanks for joining us.</Text>
                 </Container>
             </Body>

9eda895b738b956f2bfe9ab470fdadfa934b29b1
Naziya Usman <naziyausmantwaiba@gmail.com>
2025-12-23 10:58:49 +0530

    create react image

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,22 @@
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-
-
+import Image from "next/image";
+import flower from '@/public/images/flower.jpg'
 export default async function Home() {
 
   const session = await getServerSession(authOptions);
-console.log(session?.user);
-
+  console.log(session?.user);
   return (
-    <div className="container">
-      <main>
-        <h1 >hello {session && <span>{session.user!.name}</span> }</h1>
-        <button className="btn ">
-          <Link href="/users"> user</Link>
-        </button>
-
-      </main>
-
-    </div>
-  );
+    <main className="relative h-screen ">
+      <Image
+        src="https://loremflickr.com/300/170/flower"
+        alt="flower"
+        fill
+        className="object-contain"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+      ></Image>
+    </main>
+  )
 }

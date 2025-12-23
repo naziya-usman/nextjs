77f9333d76200c078444a044e6b67e0e2503cba8
Naziya Usman <naziyausmantwaiba@gmail.com>
2025-12-23 15:13:57 +0530

    use fond from google fonds

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,17 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
   console.log(session?.user);
   return (
-    <main className="relative h-screen ">
-      <Image
-        src="https://loremflickr.com/300/170/flower"
-        alt="flower"
-        fill
-        className="object-contain"
-        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-        quality={100}
-      ></Image>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold mb-8">Welcome to the Home Page</h1>
+      {session?.user ? (
+        <div >
+          <p className="mb-4 font-bold">Signed in as {session.user.email}</p>
+        </div>
+      ) : (
+        <div>
+          <p className="mb-4">Not signed in</p>
+        </div>
+      )}
     </main>
   )
 }
